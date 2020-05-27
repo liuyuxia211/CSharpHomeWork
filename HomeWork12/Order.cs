d49e61ac2aa9f5d0a826b6674c95b12a22acf753
yuxia liu <1138310046@qq.com>
2020-05-27 17:10:50 +0800

    delete HomeWork12/

--- HomeWork12/Order.cs
+++
@@ -1,5 +0,0 @@
-using System.Collections.Generic;
-public class Order{
-    public string ID{get;set;}
-    public List<OrderItem> orderItems;
-}
--- HomeWork12/OrderItem.cs
+++
@@ -1,5 +0,0 @@
-public class OrderItem{
-    public string ID{get;set;}
-    public string OrderID{get;set;}
-    public string NAME{get;set;}
-}
